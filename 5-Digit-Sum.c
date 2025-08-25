11283252f5c8c449776d6bfe66c4b1861c8256d0
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-08-25 18:13:26 +0530

    Create 5-Digit-Sum.c

---
+++ 5-Digit-Sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, d1, d2, d3, d4, d5, sum;
+    printf("Enter the five digit number:- ");
+    scanf("%d", &num);
+
+    d1 = num % 10;
+    num = num / 10;
+    d2 = num % 10;
+    num = num / 10;
+    d3 = num % 10;
+    num = num / 10;
+    d4 = num % 10;
+    num = num / 10;
+    d5 = num;
+
+    sum = d1 + d2 + d3 + d4 + d5;
+
+    printf("The sum of the five digits is %d", sum);
+
+    return 0;
+}
