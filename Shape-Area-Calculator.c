75036851d9a5fdc779539601e68a6a08341052f0
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-08-30 12:06:26 +0530

    Update Shape-Area-Calculator.c

--- Shape-Area-Calculator.c
+++ Shape-Area-Calculator.c
@@ -5,7 +5,7 @@ int main() {
     int choice;
     float length, breadth, side, radius, area;
 
-    printf("\nArea Conversion Program\nEnter 1 for Rectangle, 2 for Square, 3 for Circle:-\n");
+    printf("\nArea/Volume Calculator Program\nEnter 1 for Rectangle, 2 for Square, 3 for Circle:-\n");
     scanf("%d", &choice);
 
     if(choice == 1) {
@@ -13,16 +13,18 @@ int main() {
         scanf("%f %f", &length, &breadth);
         area = length * breadth;
         printf("\nThe area of the rectangle is %.2f units.\n\n", area);
-    } else if (choice == 2) {
+    } else if(choice == 2) {
         printf("\nSquare Area Calculator\nEnter the side of the square:-\n");
         scanf("%f", &side);
         area = pow(side, 2.0);
         printf("\nThe area of the square is %.2f units.\n\n", area);
-    } else if (choice == 3) {
+    } else if(choice == 3) {
         printf("\nCircle Area Calculator\nEnter the radius of the circle:-\n");
         scanf("%f", &radius);
         area = 3.14159 * pow(radius, 2.0);
         printf("\nThe area of the circle is %.2f units.\n\n", area);
+    } else {
+        printf("\nPlease enter the correct digit!\n\n");
     }
 
     return 0;
