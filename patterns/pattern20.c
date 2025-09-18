69cad353d3652262b787ebc5f782990cda33ba45
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:13:01 +0530

    Create pattern20.c

---
+++ patterns/pattern20.c
@@ -0,0 +1,23 @@
+// 1 2 3 4 5 
+// 2 2 3 4 5 
+// 3 3 3 4 5 
+// 4 4 4 4 5 
+// 5 5 5 5 5
+
+#include<stdio.h>
+int main () {
+	int num;
+	scanf("%d", &num);
+	for(int i = 1; i <= num; i++) {
+		for(int j = 1; j <= num; j++) {
+			if(j <= i) {
+				printf("%d ", i);
+			} else {
+				printf("%d ", j);
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
