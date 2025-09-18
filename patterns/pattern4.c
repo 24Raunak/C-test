3077bf8dff6b66e98f7f232c99fb25c2dd40e6cd
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:23:54 +0530

    Update pattern4.c

--- patterns/pattern4.c
+++ patterns/pattern4.c
@@ -4,9 +4,13 @@
 // *
 
 #include <stdio.h>
-int main() {
-    for(int i = 0; i < 4; i++) {
-        for(int j = 3; j >= i; j--) {
+
+void main() {
+    int num;
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++) {
+        for(int j = num; j >= i; j--) {
             printf("* ");
         }
         printf("\n");
