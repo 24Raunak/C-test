88c331b7ea81779d1d8105f0bb1dd711c93d8b46
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:01:23 +0530

    Create pattern14.c

---
+++ patterns/pattern14.c
@@ -0,0 +1,26 @@
+ // 1******
+ // 12*****
+ // 123****
+ // 1234***
+ // 12345**
+ // 123456*
+ // 1234567
+
+#include<stdio.h>
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	for(int i = 1; i <= num; i++) {
+		for(int j = 1; j <= num; j++) {
+			if(j == i || j <= i) {
+				printf("%d", j);
+			} else {
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
