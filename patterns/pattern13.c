63c4910155e90f010745c81243eaab3e8712fa89
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:08:38 +0530

    Update pattern13.c

--- patterns/pattern13.c
+++ patterns/pattern13.c
@@ -1,15 +1,36 @@
-#include <stdio.h>
+//       1 
+//     2 3 2
+//   3 4 5 4 3
+// 4 5 6 7 6 5 4
 
+#include<stdio.h>
 int main() {
-    for(int i = 1; i <= 9; i ++) {
-        for(int j = 1; j <= 9; j++) {
-            if(i + j == 6 || j == i + 4) {
-                printf("* ");
-            } else {
-                printf("  ");
-            }
-        } printf("\n");
-    }
+    int num;
+    scanf("%d", &num);
 
-    return 0;
+    int a = 1;
+    for(int i = 1; i <= num; i++) {
+        int b = (i * 2 - 2);
+        for(int j = 1; j <= (2 * num - 1); j++) {
+            if (j <= num) {
+                if((i + j) >= (num + 1)) {
+                    printf("%d\t", a);
+                    a ++;
+                } else {
+                    printf("\t");
+                }
+            } else {
+                if((j - i) <= num - 1) {
+                    printf("%d\t", b);
+                    b--;
+                } else {
+                    printf("\t");
+                }
+                a = i + 1;
+            }
+        }
+
+        printf("\n");
+    }
+	return 0;
 }
