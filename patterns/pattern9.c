42ddfb600e1fd10b65e215ef9eaea0d89fcb467e
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:28:08 +0530

    Update pattern9.c

--- patterns/pattern9.c
+++ patterns/pattern9.c
@@ -1,14 +1,19 @@
 // 1
-// 1 2
-// 1 2 3
-// 1 2 3 4
+// 2 3
+// 4 5 6
+// 7 8 9 10
 
 #include <stdio.h>
 
 void main() {
-    for(int i = 1; i <= 4; i++) {
+    int num;
+    scanf("%d", &num);
+
+    int x = 1;
+    for(int i = 1; i <= num; i++) {
         for(int j = 1; j <= i; j++) {
-            printf("%d ", j);
+            printf("%d ", x);
+            x++;
         }
         printf("\n");
     }
