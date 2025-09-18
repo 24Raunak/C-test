742228ab52bf9bae6e5e0a08694ca8fd262d7833
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:26:30 +0530

    Update pattern7.c

--- patterns/pattern7.c
+++ patterns/pattern7.c
@@ -1,15 +1,17 @@
 // * * * *
-// *     *
-// *     *
+// * *   *
+// *   * *
 // * * * *
 
 #include <stdio.h>
 
-int main() {
-    
-    for(int i = 1; i <= 4; i++) {
-        for(int j = 1; j <= 4; j++) {
-            if(i == 1 || i == 4 || j == 1 || j == 4) {
+void main() {
+    int num;
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i++) {
+        for(int j = 1; j <= num; j++) {
+            if(i == 1 || i == num || j == 1 || j == num || i == j) {
                 printf("* ");
             } else {
                 printf("  ");
@@ -17,6 +19,4 @@ int main() {
         }
         printf("\n");
     }
-
-    return 0;
 }
