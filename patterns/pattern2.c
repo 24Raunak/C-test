e9914b56c4e971af02b8ccef094ba5aa8593514e
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:22:02 +0530

    Update pattern2.c

--- patterns/pattern2.c
+++ patterns/pattern2.c
@@ -4,12 +4,15 @@
 // * * * *
 
 #include <stdio.h>
+
 void main() {
+    int num;
+    scanf("%d", &num)
+
     for(int i = 0; i < 4; i++) {
         for(int j = 0; j <= i; j++) {
             printf("* ");
         }
         printf("\n");
     }
-
 }
