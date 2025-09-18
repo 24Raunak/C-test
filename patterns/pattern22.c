98d925cad4a5fe3c452faecd515cb1d709b810c6
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:30:35 +0530

    Create pattern22.c

---
+++ patterns/pattern22.c
@@ -0,0 +1,24 @@
+// * * * *
+//     * * *
+//         * *
+//             *
+
+
+#include <stdio.h>
+
+void main() {
+    int num;
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i ++) {
+        for(int j = 1; j <= num; j++) {
+            if(i <= j) {
+                printf("* ");
+            } else {
+                printf("    ");
+            }
+        }
+        
+        printf("\n");
+    }
+}
