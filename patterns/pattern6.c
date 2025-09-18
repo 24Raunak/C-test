f4a536b1ffcb3e42037ffe8d2a2ad1631babf8ae
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:25:36 +0530

    Update pattern6.c

--- patterns/pattern6.c
+++ patterns/pattern6.c
@@ -1,21 +1,22 @@
-// * * *
-// *   *
-// * * *
+// * * * *
+// *     *
+// *     *
+// * * * *
 
 #include <stdio.h>
 
-int main() {
+void main() {
+    int num;
+    scanf("%d", &num);
 
-    for(int i = 1; i <= 3; i++) {
-        for(int j = 1; j <= 3; j++) {
-            if(i == 2 && j == 2) {
-                printf("  ");
-            } else {
+    for(int i = 1; i <= num; i++) {
+        for(int j = 1; j <= num; j++) {
+            if(i == 1 || i == num || j == 1 || j == num) {
                 printf("* ");
+            } else {
+                printf("  ");
             }
         }
         printf("\n");
     }
-
-    return 0;
 }
