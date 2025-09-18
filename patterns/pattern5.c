9f112026c4d6de9bdca93f74654557ef664dbaab
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:24:13 +0530

    Update pattern5.c

--- patterns/pattern5.c
+++ patterns/pattern5.c
@@ -6,9 +6,12 @@
 
 #include <stdio.h>
 
-int main() {
-    for(int i = 1; i <= 4; i ++) {
-        for(int j = 1; j <= 4; j++) {
+void main() {
+    int num;
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i ++) {
+        for(int j = 1; j <= num; j++) {
             if(i <= j) {
                 printf("* ");
             } else {
@@ -17,6 +20,4 @@ int main() {
         }
         printf("\n");
     }
-
-    return 0;
 }
