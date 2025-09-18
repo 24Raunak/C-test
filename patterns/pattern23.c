b0a1d00b8ae1ee8570015eafae5d8e060db938f4
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:30:47 +0530

    Create pattern23.c

---
+++ patterns/pattern23.c
@@ -0,0 +1,27 @@
+// *
+// * *
+// * * *
+// * * * *
+// * * *
+// * *
+// *
+
+#include <stdio.h>
+
+void main() {
+    int num;
+    scanf("%d", &num);
+
+    for(int i = 1; i <= (2 * num - 1); i++) {
+        if(i <= num) {
+            for(int j = 1; j <= i; j++) {
+                printf("* ");
+            }
+        } else {
+            for(int j = 2 * num - i; j >= 1; j--) {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+}
