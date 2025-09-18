344a686d4544edede4caa86609ba9f33600f18dd
24Raunak <79973936+24Raunak@users.noreply.github.com>
2025-09-18 23:11:33 +0530

    Create pattern18.c

---
+++ patterns/pattern18.c
@@ -0,0 +1,28 @@
+// 1 
+// 11
+// 111 
+// 1001 
+// 11111
+// 100001
+
+#include<stdio.h>
+int main() {
+	int num;
+	scanf("%d", &num);
+	for(int i = 1; i <= num; i++) {
+		for(int j = 1; j <= i; j++) {
+			if(i % 2 == 0) {
+				if(j == 1 || j == i) {
+					printf("1");
+				} else {
+					printf("0");
+				}
+			} else {
+				printf("1");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
